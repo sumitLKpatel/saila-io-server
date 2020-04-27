940430b0965ea58b6a9d7764ee812c63b3b02faf
sumitLKpatel <sumit117.it@gmail.com>
2020-04-27 15:58:06 +0530

    make it better test2

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ const init = async () => {
                 const {
                     payload
                 } = request
+                console.log(payload.ref)
+                console.log(payload.event)
+                console.log(JSON.stringify(payload.data))
                 io.of(`/${payload.ref}`).emit(payload.event, payload.data)
                 return h.response({data : 'ok'}).code(201)
             }
