9041596fdf728a78635905a80f60d247a844779f
sumitLKpatel <sumit117.it@gmail.com>
2020-04-30 14:04:58 +0530

    change7

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ const init = async () => {
                 console.log(payload)
                 // console.log(payload)
                 // console.log(JSON.stringify(payload.data))
-                const nsp = io.of('/5972247b42b4a32122357c4d');
-                nsp.emit(`${payload.event}`, payload.data)
-                // io.of(`/${payload.ref}`).emit(`${payload.event}`, payload.data)
+                // const nsp = io.of('/5972247b42b4a32122357c4d');
+                // nsp.emit(`${payload.event}`, payload.data)
+                io.of(`/${payload.ref}`).emit(`${payload.event}`, payload.data)
                 return h.response({data : 'ok'}).code(201)
             }
         },
